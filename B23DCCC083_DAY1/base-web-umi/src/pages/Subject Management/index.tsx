6d5e527bc340ef5bf6ca60ae78eed41c969306ef
JavaScript-HTML-CSS <HungTD.B23CC083@stu.ptit.edu.vn>
2025-02-20 11:35:58 +0700

    Commit

---
+++ B23DCCC083_DAY1/base-web-umi/src/pages/Subject Management/index.tsx
@@ -0,0 +1,12 @@
+import SubjectManagement from '@/components/SubjectManagement';
+
+const SubjectManagementPage = () => {
+  return (
+    <div>
+      <SubjectManagement />
+    </div>
+  );
+};
+
+export default SubjectManagementPage;
+
